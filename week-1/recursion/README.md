b2558003ffa0df14f4aa129045b3715e583ac81b
RattlePenguin <viggo.setiawan@gmail.com>
2026-01-20 23:46:39 +0800

    Added basic recursion example printList in week-1.

---
+++ week-1/recursion/README.md
@@ -0,0 +1,3 @@
+# printList.c
+### printList has been implemented recursively, can you try creating the linked list recursively too?
+### printList prints an additional space and no newline at its last member, how can you fix this?
---
+++ week-1/recursion/printList.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Linked List
+struct node {
+	int value;
+	struct node *next;
+};
+
+void printList(struct node *head);
+
+int main(void) {
+	// Create a linked list of length 5 in ascending order {1,2...5}
+	struct node *head = NULL;
+	for (int i = 0; i < 5; ++i) {
+		struct node *new = malloc(sizeof(*new));
+		new->value = 5 - i;
+		new->next = head;
+		head = new;
+	}
+
+	// Print the linked list
+	printList(head);
+
+	return 0;
+}
+
+void printList(struct node *head) {
+	if (head == NULL) {
+		return;
+	}
+
+	printf("%d ", head->value);
+
+	printList(head->next);
+}
