9b5f4a32d5a2b575221330499a9e7cedd6f83351
RattlePenguin <viggo.setiawan@gmail.com>
2026-02-03 02:55:24 +0800

    Modified week-01 to accommodate a bit of the tutorial exercises.
    Better examples in malloc.c.

--- week-01/memory/malloc.c
+++ week-01/memory/malloc.c
@@ -1,15 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int *stackInt(void);
+int *heapInt(void);
+
 int main(void) {
-	int stackInt = 5;
-	printf("stackInt == %d\n", stackInt);
+	int *myStackInt = stackInt();
+	printf("myStackInt == %d\n", *myStackInt);
 	
-	int *heapInt = malloc(sizeof(int));
-	*heapInt = 10;
+	int *myHeapInt = heapInt();
 
-	printf("heapInt == %p\n", (void *) heapInt);
-	printf("*heapInt == %d\n", *heapInt);
+	printf("myHeapInt == %p\n", (void *) myHeapInt);
+	printf("*myHeapInt == %d\n", *myHeapInt);
 
+	free(myHeapInt);
 	return 0;
+}
+
+int *stackInt(void) {
+	int a = 5;
+	return &a;
+}
+
+int *heapInt(void) {
+	int *a = malloc(sizeof(int));
+	*a = 10;
+	return a;
 }
--- week-01/pointers/ptrArithmetic.c
+++ week-01/pointers/ptrArithmetic.c
@@ -2,14 +2,25 @@
 #include <stdlib.h>
 
 int main(void) {
+	// Make an array of size 5.
 	int arr[5] = { 1, 2, 3, 4, 5 };
 
+	// Print the array in order.
+	printf("arr = { ");
 	for (int i = 0; i < 5; ++i) {
 		printf("%d ", arr[i]);
 	}
+	printf("}");
 	printf("\n");
 
 	printf("arr[0] = %d\n", arr[0]);
 	printf("(arr + 1)[0] = %d\n", (arr + 1)[0]);
+
+	// An array is just a pointer to the first element of the array.
+	int *myArrPtr = arr + 3;
+	printf("myArrPtr = arr + 3");
+	printf("\n");
+	printf("myArrPtr[0] = arr[3] = %d\n", myArrPtr[0]);
+
 	return EXIT_SUCCESS;
 }
