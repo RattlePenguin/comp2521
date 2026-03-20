c787584180a5d109c0b1d7d70a00e7578fc17b73
RattlePenguin <viggo.setiawan@gmail.com>
2026-03-20 11:31:01 +1100

    Updated week 5 tree ex

--- week-05/trees/main.c
+++ week-05/trees/main.c
@@ -4,6 +4,10 @@ int main(void) {
 	AvlTree tree = AvlTreeNew();
 	
 	AvlTreeInsert(tree, 0);
+	AvlTreeInsert(tree, 1);
+	AvlTreeInsert(tree, 2);
+	AvlTreeInsert(tree, 3);
+	AvlTreeInsert(tree, 4);
 
 	AvlTreeFree(tree);
 }
