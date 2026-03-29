9b927b16c9bc54389b6c282c61fef0566ad5d410
RattlePenguin <viggo.setiawan@gmail.com>
2026-03-29 23:31:17 +1100

    Added isEulerPath exercise week 7 unsupervised.

---
+++ week-07/graphs/isEulerPath.c
@@ -0,0 +1,4 @@
+// check whether a given path is a Euler path
+bool isEulerPath(Graph g, struct edge e[], int nE) {
+	return false;
+}
---
+++ week-07/graphs/solutions/isEulerPath.c
@@ -0,0 +1,32 @@
+bool isEulerPath(Graph g, struct edge e[], int nE) {
+	assert(g != NULL);
+
+	// includes all edges
+	if (g->nE != nE) {
+		return false;
+	}
+
+	// edges are actually in the graph
+	for (int i = 0; i < nE; i++) {
+		if (g->edges[e[i].v][e[i].w] == 0) {
+			return false;
+		}
+	}
+
+	// is actually a path
+	for (int i = 0; i < nE - 1; i++) {
+		if (e[i].w != e[i + 1].v) {
+			return false;
+		}
+	}
+
+	// includes edges exactly once
+	for (int i = 0; i < nE; i++) {
+		for (int j = i + 1; j < nE; j++) {
+			if (e[i].v == e[j].v && e[i].w == e[j].w) return false;
+			if (e[i].v == e[j].w && e[i].w == e[j].v) return false;
+		}
+	}
+	
+	return true;
+}
